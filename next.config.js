ef16c3a4b50510753b1c13e9da470de42c24ed39
박윤환(YunHwan Park) <yh107@coontec.com>
2023-12-19 19:37:02 +0900

    init: styled-components 설치

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true, // 추가
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../styles/GlobalStyle";
+import { darkTheme, lightTheme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
